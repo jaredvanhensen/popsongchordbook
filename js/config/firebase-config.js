d5d382be013884f35117d1301af33f97fbeb2b36
Jared van Hensen <jared@vanhensen.nl>
2026-01-05 11:25:35 +0100

    Update Firebase config met nieuw project

--- js/config/firebase-config.js
+++ js/config/firebase-config.js
@@ -18,14 +18,13 @@
 
 // Firebase configuratie object (gebruikt met Firebase Compat SDK)
 const firebaseConfig = {
-  apiKey: "AIzaSyBxIFvQtxrxv_jYNeJxA8juBLIpIQq4X0w",
-  authDomain: "popsongchordbook.firebaseapp.com",
-  databaseURL: "https://popsongchordbook-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "popsongchordbook",
-  storageBucket: "popsongchordbook.firebasestorage.app",
-  messagingSenderId: "443671244182",
-  appId: "1:443671244182:web:98964f2fb2f174997041b7",
-  measurementId: "G-G9SJFCVE9Q"
+  apiKey: "AIzaSyDaDli2YxzH4uXMilbRLDIByAG9AAGHcjo",
+  authDomain: "popsongchordbook-jared.firebaseapp.com",
+  databaseURL: "https://popsongchordbook-jared-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "popsongchordbook-jared",
+  storageBucket: "popsongchordbook-jared.firebasestorage.app",
+  messagingSenderId: "841480252142",
+  appId: "1:841480252142:web:5905b833ec1e29e339a1ff"
 };
 
 // reCAPTCHA Site Key voor App Check
