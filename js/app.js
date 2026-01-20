d03979af939ba773a5b81b73fb90914617227a87
Jared van Hensen <jared@vanhensen.nl>
2026-01-20 17:47:54 +0100

    Fix handleSetlistChange to call updateSetlistSelect correctly (v0.90)

--- js/app.js
+++ js/app.js
@@ -55,7 +55,7 @@ class App {
     }
 
     async init() {
-        console.log("Pop Song Chord Book - App Initialized (v0.89)");
+        console.log("Pop Song Chord Book - App Initialized (v0.90)");
         // Initialize Firebase
         try {
             await this.firebaseManager.initialize();
