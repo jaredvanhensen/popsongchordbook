b684a42b51ca5033c866f727385483c4ef4b7358
Jared van Hensen <jared@vanhensen.nl>
2026-01-20 10:28:10 +0100

    Fix version increment script and ensure v0.63 is deployed

--- increment_version.ps1
+++ increment_version.ps1
@@ -1,17 +1,18 @@
 $file = Join-Path $PSScriptRoot "index.html"
 $content = Get-Content $file -Raw -Encoding UTF8
 
-if ($content -match 'Version <span id="site-version">(\d+\.?\d*)</span>') {
+if ($content -match '<span id="site-version">(\d+\.?\d*)</span>') {
     $currentVersion = [double]$matches[1]
     $newVersion = "{0:N2}" -f ($currentVersion + 0.01)
     # Ensure dot separator for consistnecy if locale differs, though input HTML has dot.
     $newVersion = $newVersion.Replace(',', '.') 
     
-    $newContent = $content -replace 'Version <span id="site-version">(\d+\.?\d*)</span>', "Version <span id=`"site-version`">$newVersion</span>"
+    $newContent = $content -replace '<span id="site-version">(\d+\.?\d*)</span>', "<span id=`"site-version`">$newVersion</span>"
     
     # Use ascii or utf8 no bom to minimize diffs
     [System.IO.File]::WriteAllText($file, $newContent)
     Write-Host "Updated version from $currentVersion to $newVersion"
-} else {
+}
+else {
     Write-Error "Could not find version pattern in index.html"
 }
