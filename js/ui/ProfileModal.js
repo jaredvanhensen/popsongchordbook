df01bcc3a788a6aca6b4a998bb2a875eda2817a9
Jared van Hensen <jared@vanhensen.nl>
2026-04-02 17:49:41 +0200

    Fix Landing Page v2.530: Add existence guard to ProfileModal.

--- js/ui/ProfileModal.js
+++ js/ui/ProfileModal.js
@@ -83,6 +83,7 @@ class ProfileModal {
     }
 
     setupEventListeners() {
+        if (!this.modal) return;
         // Avatar upload
         if (this.changeAvatarBtn && this.avatarInput) {
             this.changeAvatarBtn.addEventListener('click', () => {
