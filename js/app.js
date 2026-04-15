0fb5b13fb7ff9efa65d3d2c3b431e0125a7fb2c6
Jared van Hensen <jared@vanhensen.nl>
2026-04-15 20:54:37 +0200

    Update application (v2.654)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Main Application (v2.624)
+﻿// Main Application (v2.624)
 class App {
     constructor() {
         // Initialize Firebase Manager first
@@ -3188,6 +3188,7 @@ class App {
 document.addEventListener('DOMContentLoaded', () => {
     window.appInstance = new App();
 });
+
 
 
 
