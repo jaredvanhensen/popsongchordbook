e41c6dbdb413dff35d53ae051f25044d862ec3df
Jared van Hensen <jared@vanhensen.nl>
2026-06-16 18:12:50 +0200

    Updated Band Sync Play button icon to 👥 emoji (v3.116)

--- js/scrolling_chords.js
+++ js/scrolling_chords.js
@@ -6769,15 +6769,7 @@ class BandSync {
             `;
             this.bandSyncBtn.title = "Synced Pause with Band";
         } else {
-            iconSpan.innerHTML = `
-                <svg viewBox="0 0 24 24" width="18" height="18" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" />
-                    <circle cx="9" cy="7" r="4" />
-                    <path d="M23 21v-2a4 4 0 0 0-3-3.87" />
-                    <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-                    <polygon points="18 13 22 16 18 19 18 13" fill="currentColor" />
-                </svg>
-            `;
+            iconSpan.innerHTML = '👥';
             this.bandSyncBtn.title = "Synced Start with Band";
         }
     }
